595b1a5ed0021866708384976234d6203fc93385
Tuyo Isaza <tuyo@aris.app>
2026-03-20 22:29:04 -0600

    fix: Import crypto module for randomBytes

--- server/scripts/maintenance/index.js
+++ server/scripts/maintenance/index.js
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import compression from 'compression';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import crypto from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
